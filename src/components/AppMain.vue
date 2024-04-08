496eeea084ab85bb9eaab8086075feb9bfc92613
mattemutti <mattemutti@gmail.com>
2024-04-08 18:10:48 +0200

    update registration section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,8 +20,30 @@ export default {
 
 		<section class="registration ">
 			<section class="container">
-				<div class="row justify-content-center ">
-					Registration
+				<div class="col-6">
+					<h2>Registration</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse debitis voluptatem
+						temporibus distinctio praesentium quis velit necessitatibus voluptatum, fugit beatae labore
+						magnam eaque est ut nisi quas. Magnam, quis!</p>
+				</div>
+				<div class="position-relative form_registration">
+					<form class=" position-absolute" action="">
+						<div class="mb-3">
+							<label for="exampleInputEmail1" class="form-label">Email address</label>
+							<input type="email" class="form-control" id="exampleInputEmail1"
+								aria-describedby="emailHelp">
+							<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+						</div>
+						<div class="mb-3">
+							<label for="exampleInputPassword1" class="form-label">Password</label>
+							<input type="password" class="form-control" id="exampleInputPassword1">
+						</div>
+						<div class="mb-3 form-check">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Check me out</label>
+						</div>
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</form>
 				</div>
 			</section>
 		</section>
